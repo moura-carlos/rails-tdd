a7a06f5fec378870d2d7a74f1ec00200e79ae276
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-01-08 10:22:39 -0800

    Added context to calculator tests

--- example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,17 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-
-  it "should return the sum of two numbers" do
-    calc = Calculator.new
-    result = calc.sum(5, 7) # calling the sum method on an instance of Calculator
-    expect(result).to eq(12) # expecting that the "result" of this sum is equal to 11
+  context '#sum' do # # => because it's an instance method
+    it 'returns the sum of two numbers' do
+      calc = Calculator.new
+      result = calc.sum(5, 7) # calling the sum method on an instance of Calculator
+      expect(result).to eq(12) # expecting that the 'result' of this sum is equal to 11
+    end
+    it 'returns the sum of two negative numbers' do
+      calculator = Calculator.new
+      sum = calculator.sum(-2, -3)
+      expect(sum).to eq(-5)
+    end
   end
-
-  it "should return the subtraction of two numbers" do
-    calc = Calculator.new
-    result = calc.subtration(10, 5)
-    expect(result).to eq(5)
+  context '#subtract' do
+    it 'returns the subtraction of two numbers' do
+      calc = Calculator.new
+      result = calc.subtration(10, 5)
+      expect(result).to eq(5)
+    end
   end
-
 end
