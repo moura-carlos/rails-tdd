35a6e16addd2739b8688c5c108a0b19cb4cab156
Carlos Moura <carloseduardo.adm26@gmail.com>
2023-01-08 13:56:19 -0800

    Created new test for NonEmptyString class that inherits from String base class. Tested to confirm that subject would take on the instance of the innermost class being tested, in this case an instance of NonEmptyString would be the one being used for the test.

--- example_rspec_tdd/spec/calculator/calculator_spec.rb
+++ example_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,23 +1,23 @@
 require 'calculator'
 
-describe Calculator do
+describe Calculator, 'about the calculator' do
   context '#sum' do # # => because it's an instance method
     it 'returns the sum of two numbers' do
-      calc = Calculator.new
-      result = calc.sum(5, 7) # calling the sum method on an instance of Calculator
+      result = subject.sum(5, 7) # calling the sum method on an instance of Calculator
       expect(result).to eq(12) # expecting that the 'result' of this sum is equal to 11
     end
     it 'returns the sum of two negative numbers' do
-      calculator = Calculator.new
-      sum = calculator.sum(-2, -3)
+      sum = subject.sum(-2, -3)
       expect(sum).to eq(-5)
     end
   end
   context '#subtract' do
     it 'returns the subtraction of two numbers' do
-      calc = Calculator.new
-      result = calc.subtration(10, 5)
+      result = subject.subtration(10, 5)
       expect(result).to eq(5)
     end
+  end
+  context '#division' do
+    it 'returns the division of two numbers'
   end
 end
